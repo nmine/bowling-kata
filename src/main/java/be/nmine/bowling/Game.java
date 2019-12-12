aecdc2d451295b7e5bf606a539345ef601c75be7
nmine <nicolas.mine11@gmail.com>
2019-12-12 13:47:00 +0100

    test game all one test all zero game

--- src/main/java/be/nmine/bowling/Game.java
+++ src/main/java/be/nmine/bowling/Game.java
@@ -1,15 +1,17 @@
 package be.nmine.bowling;
 
 public class Game {
-
-    private int score;
+    private int[] rolls = new int[21];
+    private int currentRollIndex = 0;
 
     public void roll(int pinDown) {
-        score += pinDown;
+        rolls[currentRollIndex++] = pinDown;
     }
 
     public int score() {
+        int score = 0;
+        for(int i = 0; i < rolls.length; i++)
+            score +=rolls[i];
         return score;
-
     }
 }
--- src/test/java/be/nmine/bowling/BowlingTest.java
+++ src/test/java/be/nmine/bowling/BowlingTest.java
@@ -15,15 +15,6 @@ public class BowlingTest {
         assertThat(game.score()).isEqualTo(0);
     }
 
-    private void rollAllZero(Game game) {
-            game.roll(0);
-    }
-
-    private void rollAllOne(Game game) {
-        for(int i = 0; i < 20; i++)
-            game.roll(1);
-    }
-
     @Test
     void all_one_should_return_20() {
         Game game = new Game();
@@ -31,5 +22,32 @@ public class BowlingTest {
         rollAllOne(game);
 
         assertThat(game.score()).isEqualTo(20);
+    }
+    
+//    @Test
+//    void roll_spare_should_score_next_roll_times_two() {
+//        Game game = new Game();
+//
+//        game.roll(5);
+//        game.roll(5);
+//        game.roll(3);
+//        rollMany(17,0,game);
+//
+//        assertThat(game.score()).isEqualTo(16);
+//
+//    }
+
+    private void rollMany(int times, int pins,Game game) {
+        for(int i = 0; i < times; i++)
+            game.roll(pins);
+    }
+
+    private void rollAllZero(Game game) {
+        game.roll(0);
+    }
+
+    private void rollAllOne(Game game) {
+        for(int i = 0; i < 20; i++)
+            game.roll(1);
     }
 }
